425179964f507e18682be3b53908d1b501be58d2
Carina-IU <118018310+Carina-IU@users.noreply.github.com>
2025-04-24 19:28:51 +0900

    INITIAL COMMIT

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //MongoURI: '//pburu:<xx>@cluster0.bmpfb68.mongodb.net/'
+    MongoURI: 'mongodb+srv://pburu:carinaspw@cluster0.bmpfb68.mongodb.net/users_db?retryWrites=true&w=majority'
+      
+}
